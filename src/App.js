a83190972feea89175561d802d66a0dbb00a8d1f
malfell <malfellows13@gmail.com>
2022-07-14 19:55:33 -0500

    completed activity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
 import './App.css';
+// import React Bootstrap button
+import Button from 'react-bootstrap/Button';
+// import React Bootstrap Nav bar
+import Nav from 'react-bootstrap/Nav'
+// import React Bootstrap form
+import Form from 'react-bootstrap/Form'
 
 
 
 function App() {
   return (
     <div className="App">
+
+      {/* Nav bar */}
+        <Nav activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="/about">About Us</Nav.Link>
+          </Nav.Item>
+      </Nav>
+
       <h1>Bootstrap Website</h1>
+      {/* adding bootstrap button */}
+      <Button variant="info">Info</Button>{' '}
+
+      <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Form Dropdown with favorite movies</Form.Label>
+            <Form.Control as="select">
+              <option>Inception</option>
+              <option>Forrest Gump</option>
+              <option>Shawshank Redemption</option>
+              <option>Spiderman: Into the Spiderverse</option>
+              <option>Avengers: Endgame</option>
+            </Form.Control>
+        </Form.Group>
+
+
+          
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+      </Form>
+
      
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import bootstrap
+    // npm install react-bootstrap bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
